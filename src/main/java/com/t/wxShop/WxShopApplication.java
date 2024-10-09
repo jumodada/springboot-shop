a903a624991e0859b27a917098148948c1e54ebf
wukunyun <864353360@qq.com>
2024-10-09 14:26:38 +0800

    project init

---
+++ src/main/java/com/t/wxShop/WxShopApplication.java
@@ -0,0 +1,13 @@
+package com.t.wxShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t/wxShop/WxShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t.wxShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
